d8e7895a70590ea76a404321192ee83a2061a4d3
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2023-01-26 10:26:33 -0300

    feat: App.js Browser Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Homepage from "./Pages/Homepage/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Homepage/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/checkout" element={<CheckoutPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
