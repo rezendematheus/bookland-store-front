36ffeba7a6c629498aa98cbd4217e277e6983205
LPandorf <leticiapandorf@gmail.com>
2023-01-28 16:24:19 -0300

    feat: links na header e rota mybooks

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import LogoHeader from "../Logo/LogoHeader";
 import Sidebar from "./Sidebar/Sidebar";
+import { Wrapper, Left, Margin, Middle, Right } from "./HeaderStyle";
+import { Link } from "react-router-dom";
 
 export default function Header(){
     function callSidebar(){
@@ -12,45 +14,16 @@ export default function Header(){
             <Left>
                 <ion-icon name="menu-outline"></ion-icon>
                 <Margin/>
-                <ion-icon name="library-outline"></ion-icon>
+                <Link to="mybooks" style={{color: '#ffffff'}}><ion-icon name="library-outline"></ion-icon></Link>
             </Left>
             <Middle>
                 <LogoHeader/>
             </Middle>
             <Right>
-                <ion-icon name="person-circle-outline"></ion-icon>
+                <Link to="/login" style={{color: '#ffffff'}}><ion-icon name="person-circle-outline"></ion-icon></Link>
                 <Margin/>
-                <ion-icon name="bag-handle-outline"></ion-icon>
+                <Link to="/cart" style={{color: '#ffffff'}}><ion-icon name="bag-handle-outline"></ion-icon></Link>
             </Right>
         </Wrapper>
     );
 }
-
-const Wrapper= styled.div`
-    width: 100vw;
-    color: white;
-    height: 90px;
-    background-color: #AC7BD2;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding: 10px;
-`;
-
-const Left= styled.div`
-    font-size: 30px;
-    padding: 20px;
-    display: flex;
-`;
-
-const Margin= styled.div`
-    margin-left: 10px;
-`;
-
-const Middle=styled.div``;
-
-const Right= styled.div`
-    font-size: 30px;
-    padding: 20px;
-    display: flex;
-`;
