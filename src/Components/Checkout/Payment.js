c5a0e84435b7b835f1fc6d63ed8df154020a27ff
rezendematheus <matheusreisrezende.2002@gmail.com>
2023-01-30 10:43:51 -0300

    feat: CheckoutPage: input masks, credit card payment front-end

--- src/Components/Checkout/Payment.js
+++ src/Components/Checkout/Payment.js
@@ -3,23 +3,23 @@ import { AiOutlineArrowDown } from "react-icons/ai"
 import styled from "styled-components"
 import Form from "./CheckoutFormComponents/Form"
 
-export default function Payment(){
+export default function Payment({creditPay}){
 
     const [tab, setTab] = useState("pix")
     return(
-        <>
+        <PaymentContainer>
             <Tabs>
-                <Tab onClick={()=> setTab("pix")}>
+                <Tab onClick={()=> setTab("pix")} selected={tab === 'pix' ? true : false}>
                     Pix
                 </Tab>
-                <Tab onClick={()=> setTab("boleto")}>
+                <Tab onClick={()=> setTab("boleto")} selected={tab === 'boleto' ? true : false}>
                     Boleto
                 </Tab>
-                <Tab onClick={()=> setTab("credito")}>
+                <Tab onClick={()=> setTab("credit")} selected={tab === 'credit' ? true : false}>
                     Cartão de crédito
                 </Tab>
             </Tabs>
-            <TabMirror>
+            <TabMirror selected={tab}>
                 {tab==="pix" && (
                     <PaymentBox>
                         <PixImage></PixImage>
@@ -32,31 +32,64 @@ export default function Payment(){
                         <p>O boleto bancário será exibido após a confirmação da compra.</p>
                     </PaymentBox>
                 )}
-                {tab==='credito' && (
-                    <></>
+                {tab==='credit' && (
+                    creditPay
                 )}
             </TabMirror>
-        </>
+        </PaymentContainer>
     )
 }
 
 const Tabs = styled.div`
     display: flex;
+    justify-content: space-evenly;
 
+    width: 100%;
     height: auto;
-    gap: 0px 7px;
+    
     font-size: 14px;
+    border:1px solid gray;
+    box-sizing: border-box;
+    padding-top: 10px;
 `
 
 const Tab  = styled.div`
+    width: 30%;
+    height: 60px;
+    box-sizing: border-box;
 
+    border-bottom: ${props => props.selected ? "5px solid green" : "none"};
+    background-color: ${props => props.selected ? "whitesmoke" : "lightgrey"};
+    text-align: center;
+    display: flex;
+    flex-direction: column-reverse;
+    padding: 5px;
+    color: black;
+    
 `
 
 const TabMirror  = styled.div`
-
+    width: 100%;
+    border:${props => props.selected === 'credit' ? "" : "1px solid gray"}
 `
 const PaymentBox = styled.div`
+    width: 100%;
+    min-height: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
+const PaymentContainer = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    width: 100%;
+    height: auto;
+    padding: 10px;
 `
 
 const PixImage = styled.div`
